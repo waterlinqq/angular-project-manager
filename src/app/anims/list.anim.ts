e766e931a533774e6144710850f311501f9bd35f
waterlinqq <a10311036@gmail.com>
2020-05-21 16:13:38 +0800

    feat(anims) list add and delete animation

---
+++ src/app/anims/list.anim.ts
@@ -0,0 +1,21 @@
+import {
+  trigger,
+  style,
+  transition,
+  animate,
+  query,
+  stagger,
+} from '@angular/animations'
+
+export const listAnim = trigger('list', [
+  transition('* => *', [
+    query(':enter', style({ opacity: 0 }), { optional: true }),
+    query(':enter', stagger(100, [animate('1s', style({ opacity: 1 }))]), {
+      optional: true,
+    }),
+    query(':leave', style({ opacity: 1 }), { optional: true }),
+    query(':leave', stagger(100, [animate('1s', style({ opacity: 0 }))]), {
+      optional: true,
+    }),
+  ]),
+])
--- src/app/project/project-list/project-list.component.ts
+++ src/app/project/project-list/project-list.component.ts
@@ -4,22 +4,25 @@ import { NewProjectComponent } from '../new-project/new-project.component'
 import { InviteComponent } from '../invite/invite.component'
 import { ConfirmDialogComponent } from 'src/app/shared/confirm-dialog/confirm-dialog.component'
 import { routeAnim } from 'src/app/anims/route.anim'
+import { listAnim } from 'src/app/anims/list.anim'
 
 @Component({
   selector: 'app-project-list',
   templateUrl: './project-list.component.html',
   styleUrls: ['./project-list.component.scss'],
-  animations: [routeAnim],
+  animations: [routeAnim, listAnim],
 })
 export class ProjectListComponent implements OnInit {
   @HostBinding('@route') state
   projects = [
     {
+      id: 1,
       name: '專案一',
       desc: '這是一個專案',
       coverImg: '/assets/img/covers/0.jpg',
     },
     {
+      id: 2,
       name: '專案二',
       desc: '這也是一個專案',
       coverImg: '/assets/img/covers/1.jpg',
@@ -29,7 +32,19 @@ export class ProjectListComponent implements OnInit {
 
   ngOnInit(): void {}
   onOpenDialog() {
-    this.dialogRef.open(NewProjectComponent, { data: { title: '創建項目' } })
+    const dialogRef = this.dialogRef.open(NewProjectComponent, {
+      data: { title: '創建項目' },
+    })
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(result)
+      const newProject = {
+        id: 3,
+        name: '專案三',
+        desc: '這還是一個專案',
+        coverImg: '/assets/img/covers/2.jpg',
+      }
+      this.projects = [...this.projects, newProject]
+    })
   }
   onInvite() {
     this.dialogRef.open(InviteComponent)
@@ -37,10 +52,13 @@ export class ProjectListComponent implements OnInit {
   onEdit() {
     this.dialogRef.open(NewProjectComponent, { data: { title: '編輯項目' } })
   }
-  onDelete() {
+  onDelete(project) {
     const dialogRef = this.dialogRef.open(ConfirmDialogComponent, {
       data: { title: '刪除項目', content: '確認刪除該項目嗎？' },
     })
-    dialogRef.afterClosed().subscribe(console.log)
+    dialogRef.afterClosed().subscribe((result) => {
+      // this.projects = this.projects.filter((prj) => prj.id !== project.id)
+      this.projects.pop()
+    })
   }
 }

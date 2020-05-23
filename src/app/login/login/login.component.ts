5802984f71ff88f4c9701fdf58ed8fd78d535a3f
waterlinqq <a10311036@gmail.com>
2020-05-23 15:20:14 +0800

    feat(login): responsive driven form

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-login',
@@ -6,7 +7,26 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  form: FormGroup
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.form = this.fb.group({
+      email: ['123@abc.com', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, this.validate]],
+    })
+  }
+  onSubmit({ value, valid }, event) {
+    // this.form.controls['email'].setValidators()
+    console.log(value)
+  }
+  validate(c: FormControl) {
+    const alphaNumeric = /[a-z]+\d+/
+    if (alphaNumeric.test(c.value)) {
+      return null
+    }
+    return {
+      passwordNotValid: '密碼必須英文開頭包含數字',
+    }
+  }
 }
